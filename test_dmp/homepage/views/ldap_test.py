74c11fac694af18d87805da20727a51e73906479
Ben Willard <benwillard17@gmail.com>
2015-03-31 17:03:09 -0600

    User Story 8 Active Directory 3 Hours
    We got active directory working. It was hard work at first, but now it
    works. We put it in the login.py and also in the def clean method.  The
    tricky part was the domain name.

--- test_dmp/homepage/views/ldap_test.py
+++ test_dmp/homepage/views/ldap_test.py
@@ -1,32 +1,56 @@
-#is this a valid user?
+from ldap3 import Server, Connection, AUTH_SIMPLE, STRATEGY_SYNC, STRATEGY_ASYNC_THREADED, SEARCH_SCOPE_WHOLE_SUBTREE, GET_ALL_INFO
+# import homepage.models as hmod
+
+domain = 'CHERITAGE\\'
 username = 'ben'
-pw = 'UConn2014'
-from ldap3 import Server, Connection, AUTH_SIMPLE, STRATEGY_SYNC, GET_ALL_INFO
-s = Server('cheritage.org', port=8889, get_info=GET_ALL_INFO)
-c = Connection(s, client_strategy = STRATEGY_SYNC,
-				user=username, firstname=givenName,
-				password=pw, authentication=AUTH_SIMPLE
-				)
-print(c)
-print(c.user)
-print(c.password)
-print(c.firstname)
+pw = 'UCon2014'
+
+try:
+    s = Server('128.187.61.39', port=8889, get_info=GET_ALL_INFO)
+    c = Connection(s, auto_bind=True, client_strategy = STRATEGY_SYNC,
+                    user=domain+username,
+                    password=pw, authentication=AUTH_SIMPLE
+                    )
+    print(c)
+
+    u.created = hmod.User.objects.get_or_create(user=username)
+    u.first_name = username
+    u.last_name = ''
+    u.email = username+'@cheritage.org'
+    u.set_password(pw)
+    u.is_staff = True
+    u.is_active = True
+    u.is_super_user = False
+    u.save()
+
+    u2 = authenticate(username, pw)
+    login(request, u2)
+
+
+except:
+    print("wat's up asdfio")
 
 
 
 #check if that is the right if statement
-# if c != None:
-# 	#now that we know it is a valid user, get the user account from Dango
-# 	u, created = hmod.User.objects.get_or_create(username=netid)
-# 	u.first_name = c.
-# 	u.last_name = c.
-# 	u.email = c.
-# 	u.set_password(pw)
-# 	u.save()
+if c != None:
+    print(c)
+else:
+    print("fasdl;")
+    # now that we know it is a valid user, get the user account from Dango
+    u.created = hmod.User.objects.get_or_create(username=username)
+    u.first_name = s.first_name
+    u.last_name = s.last_name
+    u.email = s.email
+    u.set_password(pw)
+    u.save()
 
-# 	#now log the user in
-# 	u2 = authenticate(netid, pw0)
-# 	login(request, u2)
+    print(s.first_name)
+    print(s.last_name)
+    print(s.username)
+    print(s.password)
+    print(s.pw)
 
-# else:
-# #run the regular login code that you already have
+    #now log the user in
+    # u2 = authenticate(netid, pw0)
+    # login(request, u2)
--- test_dmp/homepage/views/login.py
+++ test_dmp/homepage/views/login.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login
 from django_mako_plus.controller import view_function
 import homepage.models as hmod
 from django_mako_plus.controller.router import get_renderer
+from ldap3 import Server, Connection, AUTH_SIMPLE, STRATEGY_SYNC, STRATEGY_ASYNC_THREADED, SEARCH_SCOPE_WHOLE_SUBTREE, GET_ALL_INFO
 
 
 templater = get_renderer('homepage')
@@ -26,8 +27,29 @@ class LoginForm(forms.Form):
 
     def clean(self):
         if len(self.errors) == 0:
-            user = authenticate(username=self.cleaned_data['username'], password=self.cleaned_data['password'])
+            try:
+                domain = 'CHERITAGE\\'
+                username = self.cleaned_data['username']
+                pw = self.cleaned_data['password']
+                print("try user is in AD")
+                s = Server('128.187.61.39', port=8889, get_info=GET_ALL_INFO)
+                c = Connection(s, auto_bind=True, client_strategy=STRATEGY_SYNC, user=domain+username, password=pw, authentication=AUTH_SIMPLE)
+                u, created = hmod.User.objects.get_or_create(username=username)
+                u.first_name = username
+                u.last_name = ''
+                u.email = username+'@cheritage.org'
+                u.set_password(pw)
+                u.is_staff = True
+                u.is_active = True
+                u.is_super_user = False
+                u.save()
+                user = authenticate(username=username, password=pw)
+            except:
+                print("not in ad try in django")
+                print("wat's up asdfio")
+                user = authenticate(username=username, password=pw)
             if user is None:
+                print("is my user nobody?")
                 raise forms.ValidationError('Invalid Username or Password')
 
 
@@ -36,13 +58,40 @@ class LoginForm(forms.Form):
 @view_function
 def loginform(request):
     params = {}
-
     form = LoginForm()
+
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
-            login(request, user)
+            domain = 'CHERITAGE\\'
+            username = form.cleaned_data['username']
+            pw = form.cleaned_data['password']
+            try:
+                s = Server('128.187.61.39', port=8889, get_info=GET_ALL_INFO)
+                c = Connection(s, auto_bind=True, client_strategy = STRATEGY_SYNC,
+                    user=domain+username,
+                    password=pw, authentication=AUTH_SIMPLE
+                    )
+                print(c)
+
+                u, created = hmod.User.objects.get_or_create(username=username)
+                u.first_name = username
+                u.last_name = ''
+                u.email = username+'@cheritage.org'
+                u.set_password(pw)
+                u.is_staff = True
+                u.is_active = True
+                u.is_super_user = False
+                u.save()
+
+                u2 = authenticate(username, pw)
+                login(request, u2)
+
+
+            except:
+                print("wat's up asdfio")
+                user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+                login(request, user)
             return HttpResponse('<script> window.location.href = "/homepage/index" </script>')
 
     params['form'] = form
